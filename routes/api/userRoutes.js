5f285588bc6e56b1934ef340abccea265c8a8f24
rekhawb <rekha.rcr@gmail.com>
2022-07-09 11:36:08 -0400

    add and remove friends

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,6 +3,10 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend
 } = require('../../controllers/userControllers');
 
 // /api/users
@@ -13,5 +17,21 @@ router.route('/:userId').get(getSingleUser);
 
 //post route
 router.route('/').post(createUser);
+
+//put route
+router.route('/:userId').put(updateUser);
+
+//delete route
+
+router.route('/:userId').delete(deleteUser);
+
+//add a friend
+
+router.route('/:userId/friends/:friendId').put(addFriend);
+
+//delete a friend
+
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
+
 
 module.exports = router;
